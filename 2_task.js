872574eb6f35ad9b02f5916786325084b155accd
Siddeque Sajeev <siddeques45@gmail.com>
2025-08-14 19:36:45 +0530

    3 completed

--- 2_task.js
+++ 2_task.js
@@ -1,28 +1,22 @@
-//      * 
-//     * * 
+//      *
+//     * *
 //    *   *
 //   *     *
 //  * * * * *
 
+for (i = 1; i <= 5; i++) {
+  res = "";
+  for (j = 5; j >= i; j--) {
+    res = res + " ";
+  }
 
-for(i=1;i<=5;i++){
-    res=""
-    for(j=5;j>=i;j--){
-        res=res+" "
-
+  for (k = 1; k <= 5; k++) {
+    if (k == 1 || k == i || i == 5) {
+      res = res + "*";
+    } else {
+      res = res + " ";
     }
-
-   
-
-    for(k=1;k<=i;k++){
-        if(k==1||k==i || i==5){
-            res=res+"* "
-        }
-        else{
-            res=res+"  "
-        }
-    }
-    console.log(res)
+    res = res + " ";
+  }
+  console.log(res);
 }
-
-
